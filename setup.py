bb9c4c63b03b6508ee473b4f7b6728d0a4e808f4
Muhammad Fiaz <contact@muhammadfiaz.com>
2023-12-16 22:56:03 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -39,6 +39,7 @@ setup(
         'setuptools==69.0.2',
         'pytest==7.4.3',
         'packaging==23.2',
+      'colorama',
     ],
     setup_requires=['pytest-runner'],
     tests_require=['pytest'],
