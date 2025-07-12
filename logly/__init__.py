6a532c537e97d95b129f3001417c09e22de210b6
Muhammad Fiaz <contact@muhammadfiaz.com>
2025-07-12 12:21:27 +0530

    Reinitialize the Project Setup

---
+++ logly/__init__.py
@@ -0,0 +1,3 @@
+from logly._logly import __version__, sum_as_string
+
+__all__ = ["__version__", "sum_as_string"]
---
+++ logly/_logly.pyi
@@ -0,0 +1,5 @@
+from __future__ import annotations
+
+__version__: str
+
+def sum_as_string(a: int, b: int) -> str: ...
